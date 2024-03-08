ab0b6aaf5a65602ab0768b78c65289669fee4aab
GeorgeB26 <c0040953@ncl.ac.uk>
2024-03-08 13:34:06 +0000

    Added initial message sending, taking user input and producing it to console.

---
+++ client.py
@@ -0,0 +1,7 @@
+# Code for everything required for the client. Ability to send messages, encrypt and decrypt sent messages.
+username = input("Enter Username: ")
+
+message = input("Enter Message: ")
+
+
+print(username, "-->", message)
