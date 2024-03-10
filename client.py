0a09fe44e4db32a486d73f475ae6e3e3ae6468d7
GeorgeB26 <c0040953@ncl.ac.uk>
2024-03-10 16:03:32 +0000

    Added Cryptography module to requirements.txt. Implemented basic encryption and decryption of inputted messages (no sending messages yet). Added initial message filtering, alerting user when nothing is submitted.

--- client.py
+++ client.py
@@ -1,7 +1,28 @@
 # Code for everything required for the client. Ability to send messages, encrypt and decrypt sent messages.
+from cryptography.fernet import Fernet
+
+key = Fernet.generate_key()
+fernet = Fernet(key)
+
+
 username = input("Enter Username: ")
+if username == "":
+    print("No username provided")
+else:
+    print("Username Accepted")
+
 
 message = input("Enter Message: ")
+if message == "":
+    print("No message provided")
+else:
+    print("Message Accepted, sending...")
 
 
-print(username, "-->", message)
+encMessage = fernet.encrypt(message.encode())
+decMessage = fernet.decrypt(encMessage).decode()
+
+
+print(message)
+print(encMessage)
+print(username, "-->", decMessage)
