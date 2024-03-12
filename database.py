40af59556a4d68fbce65c48823aa6861d3d0fb20
Youhengfeng <c3021285@newcastle.ac.uk>
2024-03-12 16:37:29 +0000

    Test of local database

---
+++ database.py
@@ -0,0 +1,25 @@
+import mysql.connector
+
+# Connect to DB
+conn = mysql.connector.connect(
+    host='localhost',
+    user='root',
+    password='WC.cw0718',
+    database='csc8208'
+)
+
+cursor = conn.cursor()
+
+# Test
+cursor.execute('SELECT * FROM devicedata')
+
+# Result
+records = cursor.fetchall()
+
+# Print
+for record in records:
+    print(record)
+
+# Close connection
+cursor.close()
+conn.close()
