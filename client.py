71d0f82e20da424076a34817ab89bb7827cd0bdf
GeorgeB26 <c0040953@ncl.ac.uk>
2024-03-12 11:56:20 +0000

    Added while loop to ensure user has entered a message before sending, rather than encrypting an empty string.

--- client.py
+++ client.py
@@ -1,27 +1,31 @@
 # Code for everything required for the client. Ability to send messages, encrypt and decrypt sent messages.
 from cryptography.fernet import Fernet
 
+
 key = Fernet.generate_key()
 fernet = Fernet(key)
+username = ""
+message = ""
 
 
-username = input("Enter Username: ")
+while username == "":
+    username = input("Enter Username: ")
+
 if username == "":
     print("No username provided")
 else:
     print("Username Accepted")
 
+while message == "":
+    message = input("Enter Message: ")
 
-message = input("Enter Message: ")
 if message == "":
     print("No message provided")
 else:
     print("Message Accepted, sending...")
 
-
 encMessage = fernet.encrypt(message.encode())
 decMessage = fernet.decrypt(encMessage).decode()
-
 
 print(message)
 print(encMessage)
