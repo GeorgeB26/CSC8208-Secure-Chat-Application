b51b42ee1b8d1b00becebe047320ba8121e76d47
Youhengfeng <c3021285@newcastle.ac.uk>
2024-03-13 16:18:40 +0000

    Test for MongoDB

--- TestDatabase.py
+++ TestDatabase.py
@@ -1,25 +1,42 @@
-import mysql.connector
+# import mysql.connector
+#
+# # Connect to DB (Local database)
+# conn = mysql.connector.connect(
+#     host='localhost',
+#     user='root',
+#     password='WC.cw0718',
+#     database='csc8208'
+# )
+#
+# cursor = conn.cursor()
+#
+# # Test
+# cursor.execute('SELECT * FROM devicedata')
+#
+# # Result
+# records = cursor.fetchall()
+#
+# # Print
+# for record in records:
+#     print(record)
+#
+# # Close connection
+# cursor.close()
+# conn.close()
 
-# Connect to DB (Local database)
-conn = mysql.connector.connect(
-    host='localhost',
-    user='root',
-    password='WC.cw0718',
-    database='csc8208'
-)
+from pymongo import MongoClient
 
-cursor = conn.cursor()
+# MongoDB Client
+client = MongoClient('localhost', 27017)
 
-# Test
-cursor.execute('SELECT * FROM devicedata')
+# Connect to db
+db = client['CSC8208']
 
-# Result
-records = cursor.fetchall()
+# Connect to collections
+collection = db['Test']
 
-# Print
-for record in records:
-    print(record)
+print("Successful")
 
-# Close connection
-cursor.close()
-conn.close()
+for doc in collection.find():
+    print(doc)
+
