b54e41a82394b90aa46aa0dcd38ba78c2b55458d
GeorgeB26 <c0040953@ncl.ac.uk>
2024-03-18 15:35:41 +0000

    Completed rudimentary zipping and encryption of submitted files.

--- client.py
+++ client.py
@@ -8,7 +8,8 @@ username = ""
 message = ""
 decMessage = ""
 filename = ""
-filezipped = ""
+zip_file_send = ""
+save_directory = ""
 
 
 def generate_key_pair():
@@ -26,9 +27,40 @@ def generate_key_pair():
 private_key, public_key = generate_key_pair()
 
 
-def zip(filename):
-    zipfile.ZipFile('send.zip', mode='w').write(filename)
+def zip_file(filename):
+    zipfile.ZipFile(zip_file_send, mode='w').write(filename)
 # TODO: Ensure this can take user input to select a file from their machine,then pass to encryption before sending.
+
+
+def unzip_file_save(dec_zip_file):
+    with zipfile.ZipFile(dec_zip_file, 'r') as zip_ref:
+        zip_ref.extractall(save_directory)
+
+
+def encrypt_file(zip_file_send, public_key):
+    return public_key.encrypt(
+        zip_file_send,
+        padding.OAEP(
+            mgf=padding.MGF1(algorithm=hashes.SHA256()),
+            algorithm=hashes.SHA256(),
+            label=None
+        )
+    )
+
+
+def decrypt_file(enc_zip_file, private_key):
+    try:
+        dec_zip_file = private_key.decrypt(
+            enc_zip_file,
+            padding.OAEP(
+                mgf=padding.MGF1(algorithm=hashes.SHA256()),
+                algorithm=hashes.SHA256(),
+                label=None
+            )
+        )
+        return dec_zip_file
+    except ValueError:
+        return "Failed to Decrypt"
 
 
 def encrypt(message, public_key):
@@ -69,16 +101,21 @@ while True:
     print("Welcome to the application, please type 'Exit' to leave.")
     while message == "":
         message = input("Enter Message: ")
+        filename = input("Enter filepath to send: ")
 
     if message == "":
         print("No message provided")
-    if message == "Exit":
+    elif message == "Exit":
         break
     else:
         print("Message Accepted, sending...")
         message = message.encode(encoding='UTF-8')
         encMessage = encrypt(message, public_key)
         decMessage = decrypt(encMessage, private_key)
+        zip_file(filename)
+        enc_zip_file = encrypt_file(zip_file_send, public_key)
+        dec_zip_file = decrypt_file(enc_zip_file,private_key)
+
         if message == decMessage:
             print("Message arrived unedited")
         else:
@@ -86,4 +123,6 @@ while True:
         print(message.decode())
         print(encMessage)
         print(username, "-->", decMessage.decode())
+        save_directory = input("Enter filepath to save sent file: ")
+        unzip_file_save(save_directory)
         message = ""
