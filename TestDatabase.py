d10967d1b57a7c0509176251bb2aef8ebfec202a
Youhengfeng <c3021285@newcastle.ac.uk>
2024-03-18 16:15:01 +0000

    Update TestDatabase.py
    Add the details of each collections.

--- TestDatabase.py
+++ TestDatabase.py
@@ -28,10 +28,11 @@
     Details of database:
         Host: localhost
         Port: 27017
-        DB: CSC8208
+        Database: CSC8208
         Username: root
         Password: WC.cw0718
 """
+from datetime import datetime, timedelta
 
 from pymongo import MongoClient
 
@@ -49,3 +50,70 @@ print("Successful")
 for doc in collection.find():
     print(doc)
 
+# Data
+device_data = {
+    "Device_id": 1,
+    "Sender": "Sender's Name or ID",
+    "GroupID": "Group's Identifier",
+    "Data": "Some data or message",
+    "Timestamp": datetime.now()
+}
+
+server_data = {
+    "Server_id": 1,
+    "Device_id": device_data['Device_id'],
+    "EncryptedData": "some_encrypted_data",
+    "Timestamp": datetime.now()
+}
+
+backup_data ={
+    "Backup_id": 1,
+    "Server_id": server_data['Server_id'],
+    "Device_id": device_data['Device_id'],
+    "EncryptedData": server_data['EncryptedData'],
+    "Timestamp": datetime.now(),
+    "Status": "status of backup"
+}
+
+chat_session_document = {
+    "Device_id": device_data['Device_id'],
+    "StartTime": datetime.now(),
+    "EndTime": datetime.now() + timedelta(hours=1)  # Assume the time delta is an hour
+}
+
+biometrics_data = {
+    "biometrics_id": 1,
+    "Device_id": device_data['Device_id'],
+    "Data": "Biometrics data"
+}
+
+group_data = {
+    "Group_id": 1,
+    "GroupName": "Group name",
+    "Creator_id": "Creator's id",
+    "Timestamp": datetime.now()
+}
+
+group_members_data = {
+    "Group_id": group_data['Group_id'],
+    "member_id": "Group members' id",
+    "Timestamp": datetime.now()
+}
+
+
+# insert the data to collections
+result_device = db.Devices.insert_one(device_data)
+result_server= db.Servers.insert_one(server_data)
+result_backup = db.Backup.insert_one(backup_data)
+result_chat_session_document = db.ChatSession.insert_one(chat_session_document)
+result_biometrics = db.Biometrics.insert_one(biometrics_data)
+result_group= db.Group.insert_one(group_data)
+result_group_members = db.GroupMembers.insert_one(group_members_data)
+
+print(f"Inserted document with id {result_device.inserted_id}")
+print(f"Inserted document with id {result_server.inserted_id}")
+print(f"Inserted document with id {result_backup.inserted_id}")
+print(f"Inserted document with id {result_chat_session_document.inserted_id}")
+print(f"Inserted document with id {result_biometrics.inserted_id}")
+print(f"Inserted document with id {result_group.inserted_id}")
+print(f"Inserted document with id {result_group_members.inserted_id}")
