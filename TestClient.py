28baf070ea24e5195dd0507f55e89386b5cb16c9
MetaAcid <artimenko.nikita@gmail.com>
2024-03-19 22:25:47 +0100

    - Added a backup server port and updated TestClient.py

--- TestClient.py
+++ TestClient.py
@@ -3,7 +3,8 @@ import threading
 import ssl
 
 HOST = '127.0.0.1'
-PORT = 65432
+PRIMARY_PORT = 65432
+BACKUP_PORT = 65433
 
 # SSL context setup
 context = ssl.create_default_context(ssl.Purpose.SERVER_AUTH)
@@ -25,22 +26,40 @@ def receive_message(s):
             break
 
 
-def start_client():
-    with socket.create_connection((HOST, PORT)) as sock:
-        with context.wrap_socket(sock, server_hostname=HOST) as secure_sock:
-            print(f'Secure connection established with {HOST}:{PORT}')
+def connect_to_server(host, port):
+    try:
+        sock = socket.create_connection((host, port))
+        secure_sock = context.wrap_socket(sock, server_hostname=host)
+        print(f'Secure connection established with {host}:{port}')
+        return secure_sock
+    except (ConnectionRefusedError, TimeoutError):
+        print(f'Failed to connect to {host}:{port}')
+        return None
 
-        while True:
-            mode = input("Choose 'register' or 'login': ")
-            if mode in ['register', 'login']:
-                username = input("Username: ")
-                password = input("Password: ")
-                auth_data = f"{username}:{password}:{mode}"
-                secure_sock.send(auth_data.encode())
-                response = secure_sock.recv(1024).decode()
-                print(response)
-                if "successful" in response:
-                    break
+
+def start_client():
+    secure_sock = connect_to_server(HOST, PRIMARY_PORT)
+
+    # Fallback to backup server if primary connection fails
+    if not secure_sock:
+        print('Trying to connect to the backup server...')
+        secure_sock = connect_to_server(HOST, BACKUP_PORT)
+
+    if not secure_sock:
+        print('Could not connect to any server.')
+        return
+
+    while True:
+        mode = input("Choose 'register' or 'login': ")
+        if mode in ['register', 'login']:
+            username = input("Username: ")
+            password = input("Password: ")
+            auth_data = f"{username}:{password}:{mode}"
+            secure_sock.send(auth_data.encode())
+            response = secure_sock.recv(1024).decode()
+            print(response)
+            if "successful" in response:
+                break
 
         threading.Thread(target=receive_message, args=(secure_sock,)).start()
 
